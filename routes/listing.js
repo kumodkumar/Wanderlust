ef5844e471afc50eed56bb35b289156f3d92a27f
kumodkumar <komododragon1314@gmail.com>
2024-08-07 11:05:52 +0530

    done

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {listingSchema} = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
-const isLoggedIn = require("../middleware.js");
+const {isLoggedIn} = require("../middleware.js");
 
 const validateListing = (req, res, next) =>{
     let {error} = listingSchema.validate(req.body);
